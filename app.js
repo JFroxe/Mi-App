dac261c66ec7d2cc0c2c999c9fd30884024b23fa
JFroxe <147384936+JFroxe@users.noreply.github.com>
2025-03-26 15:47:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js')
-      .then(reg => console.log('✅ Service Worker registrado', reg))
-      .catch(err => console.error('❌ Error registrando Service Worker', err));
-  });
-}
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  console.log('👍 Se puede instalar la PWA en el móvil');
+
+  // Esperar 3 segundos antes de mostrar el popup
+  setTimeout(() => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('✅ PWA instalada');
+      } else {
+        console.log('❌ Instalación cancelada');
+      }
+      deferredPrompt = null;
+    });
+  }, 3000);
+});
