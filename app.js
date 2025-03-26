169e5c80a587f4f2d45a6706d723c4394b7f4da7
JFroxe <147384936+JFroxe@users.noreply.github.com>
2025-03-26 13:13:08 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(reg => console.log('✅ Service Worker registrado', reg))
+      .catch(err => console.error('❌ Error registrando Service Worker', err));
+  });
+}
